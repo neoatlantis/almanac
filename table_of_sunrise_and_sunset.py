b9f3984f73899fdc64a8244cacbdfb5713f8dfe4
Chaobai Li <contact@chaobai.li>
2019-08-09 00:03:43 +0200

    twilight calc

--- table_of_sunrise_and_sunset.py
+++ table_of_sunrise_and_sunset.py
@@ -23,6 +23,17 @@ timescale = load.timescale()
 BJT = timezone("Asia/Shanghai")
 
 
+def sunrise_sunset(ephemeris , topos, crit=-0.8333):
+    sun = ephemeris['sun']
+    topos_at = (ephemeris['earth'] + topos).at
+
+    def is_sun_up_at(t):
+        """Return `True` if the sun has risen by time `t`."""
+        t._nutation_angles = iau2000b(t.tt)
+        return topos_at(t).observe(sun).apparent().altaz()[0].degrees > crit
+
+    is_sun_up_at.rough_period = 0.5  # twice a day
+    return is_sun_up_at
 
 
 
@@ -39,27 +50,35 @@ locations = [
 
 
 
-founds = {}
-for year, month, day in listDates(YEAR):
-    utcStart = timescale.utc(year, month, day, 0, 0, 0)
-    utcEnd = timescale.utc(year, month, day, 23, 59, 59) 
-    
-    founds[(month, day)] = {}
-
-    for calcLat, calcTopo in locations:
-        t, y = almanac.find_discrete(
-            utcStart,
-            utcEnd, 
-            almanac.sunrise_sunset(ephemeris421, calcTopo)
-        )
-
-        founds[(month, day)][calcLat] = {'rise': None, 'set': None}
-        for ti, yi in zip(t, y):
-            founds[(month, day)][calcLat]["rise" if yi else "set"] = ti
-
-
 utcStart = timescale.utc(YEAR, 1, 1)
 utcEnd   = timescale.utc(YEAR, 12, 31, 23, 59, 59)
+founds = {
+    -0.8333: {},
+    -6: {},
+    -18: {},
+}
+
+for crit in founds:
+    print("Calculating: sun is below horizont with %f degrees." % -crit)
+
+    for year, month, day in listDates(YEAR):
+        utcStart = timescale.utc(year, month, day, 0, 0, 0)
+        utcEnd = timescale.utc(year, month, day, 23, 59, 59) 
+        
+        founds[crit][(month, day)] = {}
+
+        for calcLat, calcTopo in locations:
+            t, y = almanac.find_discrete(
+                utcStart,
+                utcEnd, 
+                sunrise_sunset(ephemeris421, calcTopo, crit)
+            )
+
+            founds[crit][(month, day)][calcLat] = {'rise': None, 'set': None}
+            for ti, yi in zip(t, y):
+                founds[crit][(month, day)][calcLat]["rise" if yi else "set"] = ti
+
+
 
 
 # print out all info
@@ -69,42 +88,49 @@ def translateTime(t):
     return t.utc_strftime("%H:%M")
 
 
-
-with CalculationResults("sunrise_and_set", YEAR) as writer:
-    
-    lastMonth = None
-
-    skipCount = 0
-    breakCount = 5
-
-    for year, month, day in list(listDates(YEAR)):
-
-        if skipCount > 0:
-            skipCount -= 1
-            continue
-        else:
-            skipCount = 10 - 1
-
-        if breakCount > 0:
-            breakCount -= 1
-        else:
-            breakCount = 4
-            writer.writeline(" & " * (2*len(locations) + 1) + " \\\\")
-
-        line = []
-        result = founds[(month, day)]
-
-        if lastMonth != month:
-            line.append(str(month))
-        else:
-            line.append("")
-        line.append(str(day))
-        lastMonth = month
-
-        for lat, _ in locations:
-            riseset = result[lat]
-            line.append(translateTime(riseset["rise"]))
-            line.append(translateTime(riseset["set"]))
+outputmapping = {
+    -0.8333: "sunrise_and_set",
+    -6: "civil_twilight",
+    -18: "astro_twilight",
+}
 
 
-        writer.writeline(" & ".join(line) + " \\\\")
+for crit in outputmapping:
+    with CalculationResults(outputmapping[crit], YEAR) as writer:
+        
+        lastMonth = None
+
+        skipCount = 0
+        breakCount = 5
+
+        for year, month, day in list(listDates(YEAR)):
+
+            if skipCount > 0:
+                skipCount -= 1
+                continue
+            else:
+                skipCount = 10 - 1
+
+            if breakCount > 0:
+                breakCount -= 1
+            else:
+                breakCount = 4
+                writer.writeline(" & " * (2*len(locations) + 1) + " \\\\")
+
+            line = []
+            result = founds[crit][(month, day)]
+
+            if lastMonth != month:
+                line.append(str(month))
+            else:
+                line.append("")
+            line.append(str(day))
+            lastMonth = month
+
+            for lat, _ in locations:
+                riseset = result[lat]
+                line.append(translateTime(riseset["rise"]))
+                line.append(translateTime(riseset["set"]))
+
+
+            writer.writeline(" & ".join(line) + " \\\\")
