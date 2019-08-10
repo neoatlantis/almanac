d84f77473bf9322c426e4bd532a3b0c0e52d7249
Chaobai Li <contact@chaobai.li>
2019-08-10 03:21:48 +0200

    conjunction and opposition calc using a derivate method. Fast but nastly, must calculate more time

--- table_of_events.py
+++ table_of_events.py
@@ -44,67 +44,80 @@ ephemeris421 = load("de421.bsp")
 timescale = load.timescale()
 BJT = timezone("Asia/Shanghai")
 
+
 Regulus         = Star(ra_hours=(10, 8, 21.98), dec_degrees=(11, 58, 3.0))      # 轩辕十四
 Aldebaran       = Star(ra_hours=(4, 35, 55.33), dec_degrees=(16, 30, 29.6))     # 毕宿五
 Spica           = Star(ra_hours=(13, 25, 11.53), dec_degrees=(-11, 9, 41.5))    # 角宿一
 
+Mercury = ephemeris421["Mercury"]
+Venus   = ephemeris421["Venus"]
+Earth   = ephemeris421["Earth"]
+Moon    = ephemeris421["Moon"]
+Mars    = ephemeris421["Mars"]
+Jupiter = ephemeris421["JUPITER BARYCENTER"]
+Saturn  = ephemeris421["SATURN BARYCENTER"]
+Uranus  = ephemeris421["Uranus Barycenter"]
+Neptune = ephemeris421["Neptune Barycenter"]
+Pluto   = ephemeris421["Pluto Barycenter"]
 
 
-class MoonConjunctionFinder:
+##############################################################################
 
-    def __init__(self, ephemeris, year):
-        observatory = api.Topos('51 N', '13 E')
+def derivate(f):
+    # A derivate decorator.
+    #
+    # First order derivate of discrete function f. The first value is kept
+    # same with the first difference result, therefore making derivate(f)
+    # the same length of f.
+    def derivate_of_f(t):
+        fs = f(t) # t is a 1-d numpy.array
+        gs = np.diff(fs, n=1)
+        np.insert(gs, -1, np.array([gs[-1]]))
+        return gs
+    return derivate_of_f
+
+
+
+class PtolemaicAspects:
+
+    CONJUNCTION = 1     # 合
+    OPPOSITION = 2      # 冲
+
+    def __init__(self, ephemeris, year, withStar):
         self.year_period = (
-            timescale.utc(YEAR, 1, 1),
-            timescale.utc(YEAR, 12, 31, 23, 59, 59)
+            timescale.utc(YEAR-1, 6, 1),
+#            timescale.utc(YEAR, 4, 30, 23, 59, 59)
+            timescale.utc(YEAR+1, 6, 30, 23, 59, 59)
         )
-        self.moon = ephemeris["moon"]
-        self.topos_at = (ephemeris["earth"]).at# + observatory).at
+        self.withStar = withStar
+        self.earth_at = ephemeris["earth"].at
 
-    def __minRA(self, star, t):
-        moonApparent = self.topos_at(t).observe(self.moon).apparent()
-        starApparent = self.topos_at(t).observe(star).apparent()
-        moonRA = moonApparent.radec('date')[0].hours
-        starRA = starApparent.radec('date')[0].hours
-        deltaRA1 = abs(moonRA - starRA)
-        deltaRA2 = 24 - deltaRA1
-        return np.amin(np.array([deltaRA1, deltaRA2]), axis=0)
-
-    def findRough(self, star):
-        def finder(t):
+    def find(self, star, rough_period):
+        
+        @derivate
+        def d_deltaRA(t):
             t._nutation_angles = iau2000b(t.tt)
-            minRA = self.__minRA(star, t)
-            return minRA < 1.0 / 60
-        finder.rough_period = 1.0 / 29
+            baseApparent = self.earth_at(t).observe(self.withStar).apparent()
+            starApparent = self.earth_at(t).observe(star).apparent()
+            baseRA = baseApparent.radec('date')[0].hours
+            starRA = starApparent.radec('date')[0].hours
+            deltaRA1 = np.abs(baseRA - starRA)
+            deltaRA2 = 24 - deltaRA1
+            return np.minimum(deltaRA1, deltaRA2)
+
+        def finder(t):
+            return d_deltaRA(t) > 0
+        finder.rough_period = rough_period
+
         t, y = almanac.find_discrete(
             self.year_period[0], self.year_period[1],
             finder
         )
-        founds = list(zip(t, y))
-        results = []
-        for i in range(0, len(founds) - 1):
-            t1 = founds[i][0]
-            t2 = founds[i+1][0]
-            type1 = founds[i][1]
-            type2 = founds[i+1][1]
-            if type1 == True and type2 == False and (t2.tt-t1.tt) < 2.0/24:
-                results.append((t1, t2))
-        return results
 
-    def findFine(self, star, startT, endT):
-        def finder(t):
-            t._nutation_angles = iau2000b(t.tt)
-            moonApparent = self.topos_at(t).observe(self.moon).apparent()
-            starApparent = self.topos_at(t).observe(star).apparent()
-            moonRA = moonApparent.radec('date')[0].hours
-            starRA = starApparent.radec('date')[0].hours
-            return moonRA > starRA
-        finder.rough_period = 24 
-        return (almanac.find_discrete(startT, endT, finder)[0][0], None)        # each event entry is written as (time, data), where time is a single Time object
-        
-
-
-
+        output = []
+        for ti, yi in zip(t, y):
+            output.append((ti, self.CONJUNCTION if yi else self.OPPOSITION))
+        return output
 
 ##############################################################################
 # Register of all found events
@@ -115,6 +128,14 @@ founds = {
         Regulus: [],
         Aldebaran: [],
         Spica: [],
+        Mercury: [],
+        Venus: [],
+        Mars: [],
+        Jupiter: [],
+        Saturn: [],
+        Uranus: [],
+        Neptune: [],
+        Pluto: [],
     },
     "moon_phases": [],
     "solarterms": [],
@@ -131,16 +152,20 @@ for datestr in moonPhases:
 
 #-----------------------------------------------------------------------------
 # sort out solarterms.json and append to register
+for solarterm in solarterms:
+    solartermTime = dateutil.parser.parse(solarterms[solarterm])
+    ts = timescale.utc(solartermTime)
+    founds["solarterms"].append((ts, solarterm))
 
 #-----------------------------------------------------------------------------
+# Find out conjunctions with a few stars
 
 if True:
-    moonConjFinder = MoonConjunctionFinder(ephemeris421, YEAR)
+    moonConjFinder = PtolemaicAspects(ephemeris421, YEAR, Moon)
     for star in founds["moon_conjunctions"]:
-        for startT, endT in moonConjFinder.findRough(star):
-            founds["moon_conjunctions"][star].append(
-                moonConjFinder.findFine(star, startT, endT)
-            )
+        for ti, yi in moonConjFinder.find(star, rough_period=29):
+            if yi != PtolemaicAspects.CONJUNCTION: continue
+            founds["moon_conjunctions"][star].append((ti, yi))
 
 ##############################################################################
 # Sort out events into month and push to table buffer
@@ -150,7 +175,7 @@ def filterEvents(source, month):
     output = []
     for t, data in source:
         utcT = t.utc_datetime()
-        if utcT.month != month: continue
+        if utcT.year != YEAR or utcT.month != month: continue
         output.append((t, utcT, data))
     return output
 
@@ -166,11 +191,21 @@ def translateMoonPhases(eventsList):
         output.append((t, utcT, ["朔", "上弦", "望", "下弦"][data]))
     return output
 
+def translateSolarterms(eventsList):
+    output = []
+    for t, utcT, solarterm in eventsList:
+        output.append((t, utcT, solarterm))
+    return output
+        
+
 for month in range(1, 13):
     monthEvents = []
 
     monthEvents += translateMoonPhases(
         filterEvents(founds["moon_phases"], month))
+
+    monthEvents += translateSolarterms(
+        filterEvents(founds["solarterms"], month))
 
     monthEvents += translateMoonConjunctionEvents(filterEvents(
         founds["moon_conjunctions"][Regulus], month), "轩辕十四")
@@ -181,6 +216,29 @@ for month in range(1, 13):
     monthEvents += translateMoonConjunctionEvents(filterEvents(
         founds["moon_conjunctions"][Spica], month), "角宿一")
 
+    monthEvents += translateMoonConjunctionEvents(filterEvents(
+        founds["moon_conjunctions"][Mercury], month), "水星")
+
+    monthEvents += translateMoonConjunctionEvents(filterEvents(
+        founds["moon_conjunctions"][Venus], month), "金星")
+
+    monthEvents += translateMoonConjunctionEvents(filterEvents(
+        founds["moon_conjunctions"][Mars], month), "火星")
+
+    monthEvents += translateMoonConjunctionEvents(filterEvents(
+        founds["moon_conjunctions"][Jupiter], month), "木星")
+
+    monthEvents += translateMoonConjunctionEvents(filterEvents(
+        founds["moon_conjunctions"][Saturn], month), "土星")
+
+    monthEvents += translateMoonConjunctionEvents(filterEvents(
+        founds["moon_conjunctions"][Uranus], month), " 天王星")
+
+    monthEvents += translateMoonConjunctionEvents(filterEvents(
+        founds["moon_conjunctions"][Neptune], month), "海王星")
+
+    monthEvents += translateMoonConjunctionEvents(filterEvents(
+        founds["moon_conjunctions"][Pluto], month), "冥王星")
 
     monthEvents.sort(key=lambda entry: entry[0].tt)
 
